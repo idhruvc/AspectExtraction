82a4259fa665cf84fa879650808988a24b86c0fd
mrakhal <mochamadrakha@gmail.com>
2019-09-16 00:17:52 +0700

    Update function name

--- multiword.py
+++ multiword.py
@@ -60,142 +60,6 @@ class StanfordNLP:
                 ress.append(ceil)
 
         return ress
-		
-    def dobj(self, sentence):  # extract multi word adverb
-        kalimat = self.nlp.word_tokenize(sentence)
-        pola = self.nlp.dependency_parse(sentence)
-        polas = self.nlp.pos_tag(sentence)
-        print(kalimat)
-        print(pola)
-        print(polas)
-        ress = []
-        ceil = []
-        new = []
-
-        for i in range(len(pola)):
-            con = 'nsubj' in pola[i][0]
-            j = i + 1
-            if con:
-                if polas[pola[i][2] - 1][1] == 'NN':
-                    new.append(pola[i][2])
-                elif polas[pola[i][1] - 1][1] == 'NN':
-                    new.append(pola[i][1])
-                for j in range(len(pola)):
-                    con2 = 'dobj' in pola[j][0]
-                    if con2:
-                        if (polas[pola[j][2] - 1][1] == 'VBD' or polas[pola[j][2] - 1][1] == 'JJ'):
-                            new.append(pola[j][2])
-                        if (polas[pola[j][1] - 1][1] == 'VBD' or polas[pola[j][1] - 1][1] == 'JJ'):
-                            new.append(pola[j][1])
-                        new = sorted(set(new))
-                        if len(new) == 3:
-                            kal1 = kalimat[new[0] - 1]
-                            kal2 = kalimat[new[1] - 1]
-                            kal3 = kalimat[new[2] - 1]
-                            ceil = kal1 + ' ' + kal2 + ' ' + kal3
-                            ress.append(ceil)
-
-        return ress
-
-    def adjcom(self, sentence):  # extract multi word adverb
-        kalimat = self.nlp.word_tokenize(sentence)
-        pola = self.nlp.dependency_parse(sentence)
-        print(kalimat)
-        print(pola)
-        ress = []
-        ceil = []
-        new = []
-
-        for i in range(len(pola)):
-            con = 'nsubj' in pola[i][0]
-            j = i + 1
-            if con:
-                new.append(pola[i][2])
-                for j in range(len(pola)):
-                    con2 = 'xcomp' in pola[j][0]
-                    if con2:
-                        new.append(pola[j][2])
-                        new.append(pola[j][1])
-                        new = sorted(set(new))
-                        kal1 = kalimat[new[0] - 1]
-                        kal2 = kalimat[new[1] - 1]
-                        kal3 = kalimat[new[2] - 1]
-                        ceil = kal1 + ' ' + kal2 + ' ' + kal3
-                        ress.append(ceil)
-
-        return ress
-
-    def compverb(self, sentence):  # extract multi word adverb
-        kalimat = self.nlp.word_tokenize(sentence)
-        pola = self.nlp.dependency_parse(sentence)
-        polas = self.nlp.pos_tag(sentence)
-        print(kalimat)
-        print(pola)
-        ress = []
-        ceil = []
-        new = []
-
-        for i in range(len(pola)):
-            con = 'nsubj' in pola[i][0]
-            j = i + 1
-            if con:
-                if polas[pola[i][2] - 1][1] == 'NN':
-                    new.append(pola[i][2])
-                elif polas[pola[i][1] - 1][1] == 'NN':
-                    new.append(pola[i][1])
-                for j in range(len(pola)):
-                    con2 = 'cop' in pola[j][0]
-                    if con2:
-                        if (polas[pola[j][2] - 1][1] == 'VBD' or polas[pola[j][2] - 1][1] == 'JJ'):
-                            new.append(pola[j][2])
-                        if (polas[pola[j][1] - 1][1] == 'VBD' or polas[pola[j][1] - 1][1] == 'JJ'):
-                            new.append(pola[j][1])
-                        new = sorted(set(new))
-                        kal1 = kalimat[new[0] - 1]
-                        kal2 = kalimat[new[1] - 1]
-                        kal3 = kalimat[new[2] - 1]
-                        ceil = kal1 + ' ' + kal2 + ' ' + kal3
-                        ress.append(ceil)
-
-        return ress
-
-    def admof(self, sentence):  # extract multi word adverb
-        kalimat = self.nlp.word_tokenize(sentence)
-        pola = self.nlp.dependency_parse(sentence)
-        polas = self.nlp.pos_tag(sentence)
-        print(kalimat)
-        print(pola)
-        print(polas)
-        ress = []
-        ceil = []
-        new = []
-
-        for i in range(len(pola)):
-            con = 'nsubjpass' in pola[i][0]
-            j = i + 1
-            if con:
-                if polas[pola[i][2] - 1][1] == 'NN':
-                    new.append(pola[i][2])
-                elif polas[pola[i][1] - 1][1] == 'NN':
-                    new.append(pola[i][1])
-                for j in range(len(pola)):
-                    con2 = 'advmod' in pola[j][0]
-                    if con2:
-                        if (polas[pola[j][2] - 1][1] == 'VBN' or polas[pola[j][2] - 1][1] == 'RB'):
-                            new.append(pola[j][2])
-                        if (polas[pola[j][1] - 1][1] == 'VBN' or polas[pola[j][1] - 1][1] == 'RB'):
-                            new.append(pola[j][1])
-                        new = sorted(set(new))
-
-                        print(new)
-                        if len(new) == 3:
-                            kal1 = kalimat[new[0] - 1]
-                            kal2 = kalimat[new[1] - 1]
-                            kal3 = kalimat[new[2] - 1]
-                            ceil = kal1 + ' ' + kal2 + ' ' + kal3
-                            ress.append(ceil)
-
-        return ress
 
     def negation(self, sentence):  # extract negation selain no
         kalimat = self.nlp.word_tokenize(sentence)
@@ -264,6 +128,161 @@ class StanfordNLP:
 
         return ress
 
+    def adjectivalModifier(self, sentence):  # extract multi word adverb
+        kalimat = self.nlp.word_tokenize(sentence)
+        pola = self.nlp.dependency_parse(sentence)
+        print(kalimat)
+        print(pola)
+        ress = []
+        ceil = []
+
+        for i in range(len(pola)):
+            con = 'amod' in pola[i][0]
+            if con:
+                in1 = pola[i][2]
+                in2 = pola[i][1]
+                kal1 = kalimat[in1 - 1]
+                kal2 = kalimat[in2 - 1]
+                ceil = kal1 + ' ' + kal2
+                ress.append(ceil)
+
+        return ress
+
+    def directObject(self, sentence):  # extract multi word adverb
+        kalimat = self.nlp.word_tokenize(sentence)
+        pola = self.nlp.dependency_parse(sentence)
+        polas = self.nlp.pos_tag(sentence)
+        print(kalimat)
+        print(pola)
+        print(polas)
+        ress = []
+        ceil = []
+        new = []
+
+        for i in range(len(pola)):
+            con = 'nsubj' in pola[i][0]
+            j = i + 1
+            if con:
+                if polas[pola[i][2] - 1][1] == 'NN':
+                    new.append(pola[i][2])
+                elif polas[pola[i][1] - 1][1] == 'NN':
+                    new.append(pola[i][1])
+                for j in range(len(pola)):
+                    con2 = 'dobj' in pola[j][0]
+                    if con2:
+                        if (polas[pola[j][2] - 1][1] == 'VBD' or polas[pola[j][2] - 1][1] == 'JJ'):
+                            new.append(pola[j][2])
+                        if (polas[pola[j][1] - 1][1] == 'VBD' or polas[pola[j][1] - 1][1] == 'JJ'):
+                            new.append(pola[j][1])
+                        new = sorted(set(new))
+                        if len(new) == 3:
+                            kal1 = kalimat[new[0] - 1]
+                            kal2 = kalimat[new[1] - 1]
+                            kal3 = kalimat[new[2] - 1]
+                            ceil = kal1 + ' ' + kal2 + ' ' + kal3
+                            ress.append(ceil)
+
+        return ress
+
+    def adjectivalComplement(self, sentence):  # extract multi word adverb
+        kalimat = self.nlp.word_tokenize(sentence)
+        pola = self.nlp.dependency_parse(sentence)
+        print(kalimat)
+        print(pola)
+        ress = []
+        ceil = []
+        new = []
+
+        for i in range(len(pola)):
+            con = 'nsubj' in pola[i][0]
+            j = i + 1
+            if con:
+                new.append(pola[i][2])
+                for j in range(len(pola)):
+                    con2 = 'xcomp' in pola[j][0]
+                    if con2:
+                        new.append(pola[j][2])
+                        new.append(pola[j][1])
+                        new = sorted(set(new))
+                        kal1 = kalimat[new[0] - 1]
+                        kal2 = kalimat[new[1] - 1]
+                        kal3 = kalimat[new[2] - 1]
+                        ceil = kal1 + ' ' + kal2 + ' ' + kal3
+                        ress.append(ceil)
+
+        return ress
+
+    def complementVerb(self, sentence):  # extract multi word adverb
+        kalimat = self.nlp.word_tokenize(sentence)
+        pola = self.nlp.dependency_parse(sentence)
+        polas = self.nlp.pos_tag(sentence)
+        print(kalimat)
+        print(pola)
+        ress = []
+        ceil = []
+        new = []
+
+        for i in range(len(pola)):
+            con = 'nsubj' in pola[i][0]
+            j = i + 1
+            if con:
+                if polas[pola[i][2] - 1][1] == 'NN':
+                    new.append(pola[i][2])
+                elif polas[pola[i][1] - 1][1] == 'NN':
+                    new.append(pola[i][1])
+                for j in range(len(pola)):
+                    con2 = 'cop' in pola[j][0]
+                    if con2:
+                        if (polas[pola[j][2] - 1][1] == 'VBD' or polas[pola[j][2] - 1][1] == 'JJ'):
+                            new.append(pola[j][2])
+                        if (polas[pola[j][1] - 1][1] == 'VBD' or polas[pola[j][1] - 1][1] == 'JJ'):
+                            new.append(pola[j][1])
+                        new = sorted(set(new))
+                        kal1 = kalimat[new[0] - 1]
+                        kal2 = kalimat[new[1] - 1]
+                        kal3 = kalimat[new[2] - 1]
+                        ceil = kal1 + ' ' + kal2 + ' ' + kal3
+                        ress.append(ceil)
+
+        return ress
+
+    def adverbialModifier(self, sentence):  # extract multi word adverb
+        kalimat = self.nlp.word_tokenize(sentence)
+        pola = self.nlp.dependency_parse(sentence)
+        polas = self.nlp.pos_tag(sentence)
+        print(kalimat)
+        print(pola)
+        print(polas)
+        ress = []
+        ceil = []
+        new = []
+
+        for i in range(len(pola)):
+            con = 'nsubjpass' in pola[i][0]
+            j = i + 1
+            if con:
+                if polas[pola[i][2] - 1][1] == 'NN':
+                    new.append(pola[i][2])
+                elif polas[pola[i][1] - 1][1] == 'NN':
+                    new.append(pola[i][1])
+                for j in range(len(pola)):
+                    con2 = 'advmod' in pola[j][0]
+                    if con2:
+                        if (polas[pola[j][2] - 1][1] == 'VBN' or polas[pola[j][2] - 1][1] == 'RB'):
+                            new.append(pola[j][2])
+                        if (polas[pola[j][1] - 1][1] == 'VBN' or polas[pola[j][1] - 1][1] == 'RB'):
+                            new.append(pola[j][1])
+                        new = sorted(set(new))
+
+                        print(new)
+                        if len(new) == 3:
+                            kal1 = kalimat[new[0] - 1]
+                            kal2 = kalimat[new[1] - 1]
+                            kal3 = kalimat[new[2] - 1]
+                            ceil = kal1 + ' ' + kal2 + ' ' + kal3
+                            ress.append(ceil)
+
+        return ress
 
 if __name__ == '__main__':
     sNLP = StanfordNLP()
